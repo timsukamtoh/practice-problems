6779db9c048a52813dccbf8b378e398929baef01
Timothy Sukamtoh <timtoh168@gmail.com>
2024-08-12 21:54:27 -0700

    binary search

---
+++ leetcode-bin-search.py
@@ -0,0 +1,16 @@
+def search(self, nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    left, right = 0, len(nums)-1
+    while left <= right:
+        mid = (left + right)//2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            right = mid
+        else:
+            left = mid + 1
+            
