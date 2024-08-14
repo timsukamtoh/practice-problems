1135974cffb168ce1ee551bf83d1baf3d9368eb6
Timothy Sukamtoh <timtoh168@gmail.com>
2024-08-14 15:58:03 -0700

    binary 2d search

--- leetcode-bin-search.py
+++ leetcode-bin-search.py
@@ -1,3 +1,4 @@
+#leetcode 704: Binary Search
 def search(self, nums, target):
     """
     :type nums: List[int]
@@ -15,3 +16,33 @@ def search(self, nums, target):
             right = mid - 1
 
     return -1
+
+###leetcode 74: Search a 2D Matrix###
+def searchMatrix(self, matrix, target):
+    """
+    :type matrix: List[List[int]]
+    :type target: int
+    :rtype: bool
+    """
+    top, bot = 0, len(matrix) - 1
+    while top <= bot:
+        print(top, bot)
+        mid = (top+bot)//2
+        print(mid)
+        if target >= matrix[mid][0] and target <= matrix[mid][len(matrix[mid])-1]:
+            left, right = 0, len(matrix[mid])-1
+            while left <= right:
+                center = (left+right)//2
+                if target == matrix[mid][center]:
+                    return True
+                elif target < matrix[mid][center]:
+                    right = center - 1
+                else:
+                    left = center + 1
+            return False
+        elif target < matrix[mid][0]:
+            bot = mid - 1
+        else:
+            top = mid + 1
+
+    return False
