03ca5a8187c65d7e3335a4ab31beb7786fb0c9fc
Timothy Sukamtoh <timtoh168@gmail.com>
2024-08-13 22:02:43 -0700

    redid binary search

--- leetcode-bin-search.py
+++ leetcode-bin-search.py
@@ -6,11 +6,12 @@ def search(self, nums, target):
     """
     left, right = 0, len(nums)-1
     while left <= right:
-        mid = (left + right)//2
-        if nums[mid] == target:
+        mid = (left+right)//2
+        if target == nums[mid]:
             return mid
-        elif nums[mid] > target:
-            right = mid
-        else:
+        elif target > nums[mid]:
             left = mid + 1
-            
+        else:
+            right = mid - 1
+
+    return -1
