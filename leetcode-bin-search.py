357f5169191e26894d974dce3bec785d57a04356
Timothy Sukamtoh <timtoh168@gmail.com>
2024-08-20 22:39:33 -0700

    first try koko eating bananas

--- leetcode-bin-search.py
+++ leetcode-bin-search.py
@@ -18,20 +18,20 @@ def search(self, nums, target):
     return -1
 
 
-# leetcode 74: Search a 2D Matrix###
+# leetcode 74: Search a 2D Matrix
 def searchMatrix(self, matrix, target):
     """
     :type matrix: List[List[int]]
     :type target: int
     :rtype: bool
     """
-    top, bot = 0, len(matrix)-1
+    top, bot = 0, len(matrix) - 1
     while top <= bot:
-        row = (top + bot)//2
+        row = (top + bot) // 2
         if matrix[row][0] <= target <= matrix[row][-1]:
-            left, right = 0, len(matrix[row])-1
+            left, right = 0, len(matrix[row]) - 1
             while left <= right:
-                col = (left + right)//2
+                col = (left + right) // 2
                 if target == matrix[row][col]:
                     return True
                 elif target < matrix[row][col]:
@@ -44,3 +44,37 @@ def searchMatrix(self, matrix, target):
         else:
             top = row + 1
     return False
+
+
+# leetcode 875: Koko Eating Bananas
+def minEatingSpeed(self, piles, h):
+    """
+    :type piles: List[int]
+    :type h: int
+    :rtype: int
+    """
+    biggest = max(piles)
+    if h <= len(piles):
+        return biggest
+
+    def totalTimeToEat(rate: int) -> int:
+        total_time = 0
+        for pile in piles:
+            if pile % rate == 0:
+                total_time += pile // rate
+            else:
+                total_time += pile // rate + 1
+        return total_time
+
+    left = 1
+    right = biggest
+    slowest = biggest
+
+    while left <= right:
+        k = (left + right) // 2
+        if totalTimeToEat(k) <= h:
+            slowest = min(k, slowest)
+            right = k - 1
+        else:
+            left = k + 1
+    return slowest
