1e1405137ac967b659107cae8a5ff8e59d5d7309
Timothy Sukamtoh <timtoh168@gmail.com>
2024-08-22 22:05:24 -0700

    completed binary find min

--- leetcode-bin-search.py
+++ leetcode-bin-search.py
@@ -78,3 +78,21 @@ def minEatingSpeed(self, piles, h):
         else:
             left = k + 1
     return slowest
+
+
+# leetcode #153. Find Minimum in Rotated Sorted Array
+def findMin(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    smallest = nums[0]
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        smallest = min(nums[mid], smallest)
+        if nums[mid] > nums[right]:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return smallest
