f8ae34ef9ef96915517a8e67361ceb0188efaa96
Timothy Sukamtoh <timtoh168@gmail.com>
2024-08-25 17:48:14 -0700

    search rotated arr first attempt

--- leetcode-bin-search.py
+++ leetcode-bin-search.py
@@ -95,5 +95,23 @@ def findMin(self, nums):
             left = mid + 1
         else:
             right = mid - 1
-
     return smallest
+
+
+# leetcode 33. Search in Rotated Sorted Array
+def search(self, nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > nums[right] and nums[right] >= target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
