9404891efb4db27173bf00ef908dac8bcd16aacc
Timothy Sukamtoh <timtoh168@gmail.com>
2024-08-26 22:40:41 -0700

    changed search in rotated target

--- leetcode-bin-search.py
+++ leetcode-bin-search.py
@@ -110,8 +110,14 @@ def search(self, nums, target):
         mid = (left + right) // 2
         if nums[mid] == target:
             return mid
-        elif nums[mid] > nums[right] and nums[right] >= target:
-            left = mid + 1
+        if nums[left] <= nums[mid]:
+            if nums[left] <= target and nums[mid] >= target:
+                right = mid - 1
+            else:
+                left = mid + 1
         else:
-            right = mid - 1
+            if nums[right] >= target and nums[mid] <= target:
+                left = mid + 1
+            else:
+                right = mid - 1
     return -1
