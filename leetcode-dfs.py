f193551c8943eb1dd0f2a3bec3c7a14a1074a165
Timothy Sukamtoh <timtoh168@gmail.com>
2024-09-19 15:08:15 -0700

    fixed curly to reg parenthesis to make it work

--- leetcode-dfs.py
+++ leetcode-dfs.py
@@ -9,15 +9,17 @@ def numIslands(self, grid):
     ROW, COL = len(grid), len(grid[0])
 
     def dfs(r, c):
-        if { r < 0 or
-            c < 0 or
-            r >= ROW or
-            c >= COL or
-            grid[r][c] == "0" or
-            (r,c) in visited}:
+        if (
+            r < 0
+            or c < 0
+            or r >= ROW
+            or c >= COL
+            or grid[r][c] == "0"
+            or (r, c) in visited
+        ):
             return
         else:
-            visited.add((r,c))
+            visited.add((r, c))
             dfs(r + 1, c)
             dfs(r - 1, c)
             dfs(r, c + 1)
@@ -25,8 +27,8 @@ def numIslands(self, grid):
 
     for r in range(ROW):
         for c in range(COL):
-            if grid[r][c] == "1" and (r,c) not in visited:
+            if grid[r][c] == "1" and (r, c) not in visited:
                 islands += 1
-                dfs(r,c)
+                dfs(r, c)
 
     return islands
