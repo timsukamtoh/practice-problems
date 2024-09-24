87e75673cfeac2714b7c9de3176f5ca153ef5911
Timothy Sukamtoh <timtoh168@gmail.com>
2024-09-23 23:46:14 -0700

    redo num islands

--- leetcode-dfs.py
+++ leetcode-dfs.py
@@ -4,7 +4,7 @@ def numIslands(self, grid):
     :type grid: List[List[str]]
     :rtype: int
     """
-    islands = 0
+    num_islands = 0
     visited = set()
     ROW, COL = len(grid), len(grid[0])
 
@@ -14,21 +14,21 @@ def numIslands(self, grid):
             or c < 0
             or r >= ROW
             or c >= COL
-            or grid[r][c] == "0"
             or (r, c) in visited
+            or grid[r][c] == "0"
         ):
             return
-        else:
-            visited.add((r, c))
-            dfs(r + 1, c)
-            dfs(r - 1, c)
-            dfs(r, c + 1)
-            dfs(r, c - 1)
+
+        visited.add((r, c))
+        dfs(r + 1, c)
+        dfs(r - 1, c)
+        dfs(r, c + 1)
+        dfs(r, c - 1)
 
     for r in range(ROW):
         for c in range(COL):
             if grid[r][c] == "1" and (r, c) not in visited:
-                islands += 1
+                num_islands += 1
                 dfs(r, c)
 
-    return islands
+    return num_islands
