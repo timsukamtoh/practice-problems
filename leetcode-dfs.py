3ae879131fb639fe7c7f2cd1b6a2497220cb95f8
Timothy Sukamtoh <timtoh168@gmail.com>
2024-09-24 22:27:37 -0700

    first pass max area of island

--- leetcode-dfs.py
+++ leetcode-dfs.py
@@ -32,3 +32,40 @@ def numIslands(self, grid):
                 dfs(r, c)
 
     return num_islands
+
+
+#
+def maxAreaOfIsland(self, grid):
+    """
+    :type grid: List[List[int]]
+    :rtype: int
+    """
+    max_area = 0
+    visited = set()
+    ROW, COL = len(grid), len(grid[0])
+
+    def dfs(r, c, curr_area):
+        if (
+            r < 0
+            or c < 0
+            or r >= ROW
+            or c >= COL
+            or grid[r][c] == 0
+            or (r, c) in visited
+        ):
+            print("exit dfs")
+            return
+        else:
+            visited.add((r, c))
+            max_area = max(max_area, curr_area + 1)
+            dfs(r + 1, c, curr_area + 1)
+            dfs(r - 1, c, curr_area + 1)
+            dfs(r, c + 1, curr_area + 1)
+            dfs(r, c - 1, curr_area + 1)
+
+    for r in range(ROW):
+        for c in range(COL):
+            if grid[r][c] == 1 and (r, c) not in visited:
+                print("enter dfs at ")
+                dfs(r, c, 0)
+    return max_area
